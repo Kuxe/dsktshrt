5d44dfbc0450255815292354b07588d2b7ac451d
Joakim Thoren <joakimthoren93@gmail.com>
2014-11-07 05:17:51 +0100

    added command-line usage

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "model.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+    Model model;
 
-	Model model;
+    if(argc == 2 && (strcmp(argv[1], "-g") == 0 || strcmp(argv[1], "--gui") == 0))
+    {
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.setModel(&model);
+        w.show();
+        return a.exec();
+    }
 
-	MainWindow w;
-	w.setModel(&model);
-	w.show();
+    if(argc == 4)
+    {
+        string name = argv[1];
+        string appPath = argv[2];
+        string iconPath = argv[3];
 
-	return a.exec();
+        model.setName(name);
+        model.setAppPath(appPath);
+        model.setIconPath(iconPath);
+        model.createShortcut();
+        return 0;
+    }
+    cout << "Usage:\tdsktshrt <appName> <appPath> <appIcon>" << endl;
+    cout << "\tdsktshrt --help\t\t Display this help and exit" << endl;
+    cout << "\tdsktshrt [-g | --gui]\t Start Graphical User Interface" << endl;
+    return 1;
 }
