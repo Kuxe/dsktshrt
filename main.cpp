df3d10c4d3043241ec515b89018a7061566c4576
Joakim Thorén <joakimthoren93@gmail.com>
2015-01-12 20:37:06 +0100

    removed cli interface

--- main.cpp
+++ main.cpp
@@ -9,30 +9,15 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     Model model;
-
-    if(argc == 2 && (strcmp(argv[1], "-g") == 0 || strcmp(argv[1], "--gui") == 0))
+    if(argc != 1)
     {
-        QApplication a(argc, argv);
-        MainWindow w;
-        w.setModel(&model);
-        w.show();
-        return a.exec();
-    }
+        cout << "No args allowed yet, try running without arguments!" << endl;
+        return 1;
 
-    if(argc == 4)
-    {
-        string name = argv[1];
-        string appPath = argv[2];
-        string iconPath = argv[3];
-
-        model.setName(name);
-        model.setAppPath(appPath);
-        model.setIconPath(iconPath);
-        model.createShortcut();
-        return 0;
     }
-    cout << "Usage:\tdsktshrt <appName> <appPath> <appIcon>" << endl;
-    cout << "\tdsktshrt --help\t\t Display this help and exit" << endl;
-    cout << "\tdsktshrt [-g | --gui]\t Start Graphical User Interface" << endl;
-    return 1;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setModel(&model);
+    w.show();
+    return a.exec();
 }
